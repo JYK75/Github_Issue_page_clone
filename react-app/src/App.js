86617593db7f70e932cc54db38d9bcaa4d9ff80a
Devbvb <vbvb4546@gmail.com>
2022-09-29 23:54:29 +0900

    esLint prettier

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,18 +1,13 @@
-import "./App.css";
-import React, { useEffect, useState } from "react";
+import styles from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    if (count === 5) console.log("this is five");
-  }, [count]);
-
   return (
-    <div className="App">
-      <span>{count}</span>
-      <button onClick={() => setCount(count + 1)}> + </button>
-    </div>
+    <>
+      <div className={styles.nav}>Nav</div>
+      <div className={styles.header}>Header</div>
+      <div className={styles.listContainer}>ListContainer</div>
+      <div className={styles.footer}>Footer</div>
+    </>
   );
 }
 
