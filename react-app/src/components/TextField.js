e77702d9210e6d26c163661989113a2cff14e9c1
Devbvb <juuyoung9575@gmail.com>
2022-10-11 21:42:29 +0900

    form control

--- react-app/src/components/TextField.js
+++ react-app/src/components/TextField.js
@@ -4,21 +4,29 @@ import { forwardRef } from "react";
 import styles from "./TextField.module.css";
 
 export default forwardRef(function TextField(
-  { type = "input", name, placeholder },
+  { type = "input", name, placeholder, onChange, value, error },
   ref,
 ) {
   return type === "input" ? (
     <input
+      onChange={onChange}
+      value={value}
       name={name}
       ref={ref}
-      className={cx(styles.input, styles.border)}
+      className={cx(styles.input, styles.border, {
+        [styles.error]: Boolean(error),
+      })}
       placeholder={placeholder}
     ></input>
   ) : (
     <textarea
+      onChange={onChange}
+      value={value}
       name={name}
       ref={ref}
-      className={cx(styles.input, styles.textarea, styles.border)}
+      className={cx(styles.input, styles.textarea, styles.border, {
+        [styles.error]: Boolean(error),
+      })}
       placeholder={placeholder}
     ></textarea>
   );
